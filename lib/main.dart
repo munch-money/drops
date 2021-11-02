594d758a4cd7f8ed87b6e8621abf9acba88aed3e
Rahul Mathur <mathur.rahul.2501@gmail.com>
2021-11-02 17:48:31 +0530

    update 0.1

--- lib/main.dart
+++ lib/main.dart
@@ -19,6 +19,52 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class Backend {
+  Future<double?> conversion(String currency, int amt, String pmtplat) async {
+    double? rate = await MoneyConverter.convert(
+        Currency(currency), Currency(Currency.INR));
+    //   return convert(rate, amt, pmtplat, currency);
+    // }
+
+    // double convert(var rate, var amt, String pmtplat, var curr) {
+    double convertedAmt = rate! * amt;
+    print(convertedAmt);
+    // prcChg(pmtplat, curr, convertedAmt);
+    return convertedAmt;
+    // return prcChg(pmtplat, curr, convertedAmt);
+  }
+
+  int prcChg(String pmtPlatform, String currency, double? finAmt) {
+    var charge;
+    if (pmtPlatform == 'Paypal') {
+      charge = 0.044;
+    } else if (pmtPlatform == 'Stripe' && currency == 'USD') {
+      charge = 0.029;
+    } else if (pmtPlatform == 'Stripe' &&
+        (currency == 'EUR' || currency == 'GBP')) {
+      charge = 0.034;
+    } else if (pmtPlatform == 'Wise (Transferwise)') {
+      charge = 0.0004;
+    } else if (pmtPlatform == 'Razorpay') {
+      charge = 0.03;
+    } else if (pmtPlatform == 'Payoneer') {
+      charge = 0.03;
+    } else if (pmtPlatform == 'Direct Bank Deposit') {
+      charge = 0.02;
+    }
+    print(charge);
+    int fee = charge * finAmt;
+    print(fee);
+    return fee;
+  }
+
+  double finalAmt(var convertedAmt, var processingFee) {
+    var finAmt = convertedAmt - processingFee;
+    print(finAmt);
+    return finAmt;
+  }
+}
+
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
   final String title;
@@ -28,19 +74,22 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  var convertedValue;
-  var cvalue = 'USD';
-  var pvalue = 'Paypal';
+  final backend = Backend();
+  double? convertedValue;
+  String cvalue = 'USD';
+  String pvalue = 'Paypal';
   var amtController = TextEditingController();
-  var pmtPlatform;
-  var currency;
-  var amt;
-  var charge;
+  String? pmtPlatform;
+  String? currency;
+  int? charge;
+
+  late int amt;
+
   var currencies = [
     'USD',
     'EUR',
     'GBP',
-    'CHF',
+    'SGD',
   ];
   var paymentProcessors = [
     'Paypal',
@@ -103,64 +152,20 @@ class _MyHomePageState extends State<MyHomePage> {
                       amt = int.parse(amtController.text);
                       print(amt);
                       // pvalue = pmtPlatform;
-                      conversion(cvalue, amt, pvalue).then((value) {
+                      backend.conversion(cvalue, amt, pvalue).then((value) {
                         setState(() {
                           convertedValue = value;
+                          charge = backend.prcChg(pvalue, cvalue, convertedValue);
                         });
                       });
                     },
                     child: Text('Go')),
-                if (convertedValue != null) Text('$convertedValue'),
+                if (convertedValue != null)
+                  Text('Initial amount: $convertedValue'),
+                Text('Processing charge : $charge'),
               ],
             )),
       ),
-    ); 
+    );
   }
 }
-
-Future<double?> conversion(var currency, int amt, var pmtplat) async {
-    double? rate = await MoneyConverter.convert(
-        Currency(currency), Currency(Currency.INR));
-    return convert(rate, amt, pmtplat, currency);
-}
-
-double convert(var rate, var amt, var pmtplat, var curr) {
-  var convertedAmt = rate * amt;
-  print(convertedAmt);
-  prcChg(pmtplat, curr, convertedAmt);
-  return convertedAmt;
-}
-
-void prcChg(var pmtPlatform, var currency, var finAmt) {
-  var charge;
-  if (pmtPlatform == 'Paypal') {
-    charge = 0.044;
-  } else if (pmtPlatform == 'Stripe' && currency == 'USD') {
-    charge = 0.029;
-  } else if (pmtPlatform == 'Stripe' &&
-      (currency == 'EUR' || currency == 'GBP')) {
-    charge = 0.034;
-  } else if (pmtPlatform == 'Wise (Transferwise)') {
-    charge = 0.0004;
-  } else if (pmtPlatform == 'Razorpay') {
-    charge = 0.03;
-  } else if (pmtPlatform == 'Payoneer') {
-    charge = 0.03;
-  } else if (pmtPlatform == 'Direct Bank Deposit') {
-    charge = 0.02;
-  }
-  print(charge);
-  prcFee(charge, finAmt);
-}
-
-void prcFee(var chg, var amt) {
-  var fee = chg * amt;
-  print(fee);
-  finalAmt(amt, fee);
-}
-
-void finalAmt(var convertedAmt, var processingFee) {
-  var finAmt = convertedAmt - processingFee;
-  print(finAmt);
-}
-
