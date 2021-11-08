6936511c7d1be16268e16b76e6e11b347619206c
Rahul Mathur <mathur.rahul.2501@gmail.com>
2021-11-08 17:29:41 +0530

    updated UI

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   }
 }
 //
-//Currency rate to INR: 
+//Currency rate to INR:
 //Payment amount (After conversion to INR): X
 //Gateway Name fee: Y
 //With fee: z
@@ -29,25 +29,32 @@ class MyApp extends StatelessWidget {
 class Backend {
   double processingFee = 0;
   double charge = 0;
-  double afterConversion = 0;
+  double foreignCharge = 0;
+  double initialAmt = 0;
+  double domesticAfterConversion = 0;
+  double foreignAfterfee = 0;
   String paymentGateway = 'Paypal';
   double rate = 0;
   double moneyReceived = 0;
 
   Future<void> conversion(String currency, double amt) async {
-    rate = await MoneyConverter.convert(Currency(currency), Currency(Currency.INR)) ?? 0;
-    afterConversion = rate * amt;
-    print(afterConversion);
+    rate = await MoneyConverter.convert(
+            Currency(currency), Currency(Currency.INR)) ??
+        0;
+    domesticAfterConversion = rate * amt;
+    initialAmt = amt;
+    print(domesticAfterConversion);
   }
 
   void prcChg(String currency) {
     var pmtPlatform = paymentGateway;
-    var finAmt = afterConversion;
+    var finAmt = domesticAfterConversion;
     if (pmtPlatform == 'Paypal') {
       charge = 0.044;
     } else if (pmtPlatform == 'Stripe' && currency == 'USD') {
       charge = 0.029;
-    } else if (pmtPlatform == 'Stripe' && (currency == 'EUR' || currency == 'GBP')) {
+    } else if (pmtPlatform == 'Stripe' &&
+        (currency == 'EUR' || currency == 'GBP')) {
       charge = 0.034;
     } else if (pmtPlatform == 'Wise (Transferwise)') {
       charge = 0.0004;
@@ -60,12 +67,14 @@ class Backend {
     }
     print(charge);
     processingFee = charge * finAmt;
+    foreignCharge = initialAmt * charge;
+    foreignAfterfee = initialAmt - foreignCharge;
     print(processingFee);
     finalAmtReceived();
   }
 
   void finalAmtReceived() {
-    moneyReceived = afterConversion - processingFee;
+    moneyReceived = domesticAfterConversion - processingFee;
   }
 
   Future<void> update(String currency, double amt) async {
@@ -98,86 +107,126 @@ class _MyHomePageState extends State<MyHomePage> {
     'GBP',
     'SGD',
   ];
-  var paymentProcessors = ['Paypal', 'Payoneer', 'Stripe', 'Wise (Transferwise)', 'Direct Bank Transfer'];
+  var paymentProcessors = [
+    'Paypal',
+    'Payoneer',
+    'Stripe',
+    'Wise (Transferwise)',
+    'Direct Bank Transfer'
+  ];
 
   @override
   Widget build(BuildContext context) {
+    print(cvalue);
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Padding(
-            padding: const EdgeInsets.all(50.0),
-            child: Column(
-              // mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                DropdownButton<String>(
-                  value: cvalue,
-                  items: currencies.map((String items) => DropdownMenuItem(value: items, child: Text(items))).toList(),
-                  onChanged: (newValue) {
-                    cvalue = newValue!;
-                    if (amtController.text.isNotEmpty) {
-                      var amt = double.parse(amtController.text);
-                      backend.update(cvalue, amt).then((value) {
-                        setState(() {});
-                      });
-                    }
-                  },
-                ),
-                DropdownButton<String>(
-                  value: backend.paymentGateway,
-                  items: paymentProcessors
-                      .map((String items) => DropdownMenuItem(value: items, child: Text(items)))
-                      .toList(),
-                  onChanged: (newValue) {
-                    backend.paymentGateway = newValue!;
-                    if (amtController.text.isNotEmpty) {
-                      var amt = double.parse(amtController.text);
-                      backend.update(cvalue, amt).then((value) {
-                        setState(() {});
-                      });
-                    }
-                  },
-                ),
-                Container(
-                  constraints: BoxConstraints(minWidth: 280, maxWidth: 480),
-                  child: TextFormField(
-                    keyboardType: TextInputType.number,
-                    controller: amtController,
-                    decoration: const InputDecoration(
-                      hintText: 'How much money are you receiving?',
-                      labelText: 'Amount',
+      // appBar: AppBar(
+      //   title: Text(widget.title),
+      // ),
+      body: SafeArea(
+        child: Center(
+          child: Container(
+            width: 600,
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  // mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('What currency are you getting paid in?'),
+                        DropdownButton<String>(
+                          value: cvalue,
+                          items: currencies
+                              .map((String items) => DropdownMenuItem(
+                                  value: items, child: Text(items)))
+                              .toList(),
+                          onChanged: (newValue) {
+                            setState(() {
+                              cvalue = newValue!;
+                            });
+                            if (amtController.text.isNotEmpty) {
+                              var amt = double.parse(amtController.text);
+                              backend.update(cvalue, amt);
+                            }
+                          },
+                        ),
+                      ],
                     ),
-                  ),
+                    Text('Which payment provider are you using?'),
+                    DropdownButton<String>(
+                      value: backend.paymentGateway,
+                      items: paymentProcessors
+                          .map((String items) => DropdownMenuItem(
+                              value: items, child: Text(items)))
+                          .toList(),
+                      onChanged: (newValue) {
+                        backend.paymentGateway = newValue!;
+                        if (amtController.text.isNotEmpty) {
+                          var amt = double.parse(amtController.text);
+                          backend.update(cvalue, amt).then((value) {
+                            setState(() {});
+                          });
+                        }
+                      },
+                    ),
+                    Text('How much are you making?'),
+                    Container(
+                      constraints: BoxConstraints(minWidth: 280, maxWidth: 480),
+                      child: TextFormField(
+                        keyboardType: TextInputType.number,
+                        controller: amtController,
+                        decoration: const InputDecoration(
+                          labelText: 'Amount',
+                        ),
+                      ),
+                    ),
+                    // Row(
+                    //   mainAxisAlignment:MainAxisAlignment.center,
+                    // children: [
+                    TextButton(
+                      onPressed: () {
+                        var amt = double.parse(amtController.text);
+                        print(amt);
+                        backend.update(cvalue, amt).then((value) {
+                          setState(() {});
+                        });
+                      },
+                      child: Text('Go'),
+                    ),
+                    // ],
+                    // ),
+                   
+                    Container(
+                      child: Text(
+                          'By using ${backend.paymentGateway}, you\'ll be paying ${(backend.charge * 100).toStringAsFixed(2)}%, approx $cvalue ${backend.foreignCharge.toStringAsFixed(2)} as payment fees. \n\nAfter charges, you will receive approx $cvalue ${backend.foreignAfterfee} which translates to INR ${backend.domesticAfterConversion.toStringAsFixed(2)} as per current exchange rates', style: Theme.of(context).textTheme.bodyText1!.copyWith(color: backend.charge != 0 ? Colors.black : Colors.grey),),
+                    ),
+                    //   Container(
+                    //     child:
+                    //         Text('Initial amount (in INR) : ${backend.domesticAfterConversion.toStringAsFixed(2)}'),
+                    //   ),
+                    //   // Text('Initial amount (in INR) : ${backend.domesticAfterConversion.toStringAsFixed(2)}'),
+                    //   SizedBox(
+                    //     height: 10,
+                    //   ),
+                    //   Text('Processing fee (in INR) : ${backend.processingFee.toStringAsFixed(2)} (${(backend.charge*100).toStringAsFixed(2)}%)'),
+                    //   SizedBox(
+                    //     height: 10,
+                    //   ),
+                    //   Text('Final amount (in INR) : ${backend.moneyReceived.toStringAsFixed(2)}'),
+                    // ],
+                    SizedBox(
+                      height: 80,
+                    )
+                  ],
                 ),
-                TextButton(
-                  onPressed: () {
-                    var amt = double.parse(amtController.text);
-                    print(amt);
-                    backend.update(cvalue, amt).then((value) {
-                      setState(() {});
-                    });
-                  },
-                  child: Text('Go'),
-                ),
-                // Card(
-                //   child:
-                //     Text('Initial amount (in INR) : ${backend.afterConversion}'),
-                //   color: Colors.blue,
-                //     ),
-                Text('Initial amount (in INR) : ${backend.afterConversion.toStringAsFixed(2)}'),
-                SizedBox(
-                  height: 10,
-                ),
-                Text('Processing fee : ${backend.processingFee.toStringAsFixed(2)}'),
-                SizedBox(
-                  height: 10,
-                ),
-                Text('Final amount : ${backend.moneyReceived.toStringAsFixed(2)}'),
-              ],
-            )),
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
